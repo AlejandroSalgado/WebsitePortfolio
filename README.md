d1e76468d1af4e3e749d67df17a2ba75a30ef201
AlejandroSalgado <47224858+AlejandroSalgado@users.noreply.github.com>
2026-03-03 22:50:17 +0000

    Updated files for running in developer codespace

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Here are your Instructions
-To run this application in developer mode run the following command: "npm start"
+To run this application in developer mode run the following command: "npm install" & "npm start"
+It needs the node version 18 to install it do "nvm install 18" "nvm use 18"
+
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GameDevPortfolio from "./components/GameDevPortfolio";
 import { Toaster } from "./components/ui/toaster";
 
+// 
 function App() {
+  //  Use this when in developer mode: <BrowserRouter basename="/">
+  // Use this to deploy:  <BrowserRouter basename="/WebsitePortfolio">
   return (
     <div className="App">
-      <BrowserRouter basename="/WebsitePortfolio">
+      <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<GameDevPortfolio />} />
         </Routes>
